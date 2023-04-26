2c370b4ada3c6926af538937654c66f0e054726d
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2023-04-25 21:02:56 -0300

    feat: exercicios e teorias com exemplos feitos do dia

---
+++ secao-1-introducao-a-python/dia-2-entrada-e-saida-de-dados/praticando_teoria.py
@@ -0,0 +1,19 @@
+name = input("Digite o seu nome: ")
+
+for letters in name:
+    print(letters)
+
+##########################
+
+nums = input("Insira valores aqui, separados por espaço: ")
+
+nums_arr = nums.split(" ")
+
+sum = 0
+for num in nums_arr:
+    if not num.isdigit():
+        print(f"Erro ao somar valores, {num} não é um dígito.")
+    else:
+        sum += int(num)
+
+print(f"A soma dos valores válidos é: {sum}")
